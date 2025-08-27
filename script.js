12d5edf7539081c57a20b9f5f3eda9460c0de399
ranameet <meetrana149@gmail.com>
2025-08-27 16:43:17 +0530

    commit

--- script.js
+++ script.js
@@ -40,3 +40,14 @@ window.onscroll=() => {
 
 };
 
+ ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+
+});
+
+ScrollReveal().reveal('.heading', { origin: 'top' });
+ScrollReveal().reveal('.home_img, .services-container,.portfolio-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.about-content', { origin: 'right' });
